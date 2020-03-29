ca93abc13574cd16bf0107515fee44a8796e5655
s16446 <s16446@pjwstk.edu.pl>
2020-03-29 16:45:30 +0200

    Zadania 6-8 dodane

--- CW3_WebApplication1/WebApplication1/Controllers/StudentsController.cs
+++ CW3_WebApplication1/WebApplication1/Controllers/StudentsController.cs
@@ -4,19 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.DAL;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
-    [ApiController]  
+    [ApiController]
     [Route("api/students")]
 
     public class StudentsController : ControllerBase
     {
-         [HttpGet]
-         public string GetStudent(string orderBy) {
-             return $"Nowak, Kowalska, Kossak sortowanie={orderBy}";
-         }
-        
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudent(string orderBy) {
+            //return $"Nowak, Kowalska, Kossak sortowanie={orderBy}";
+            return Ok(_dbService.GetStudents());
+        }
+
         [HttpGet("{id}")]
         public IActionResult GetStudent(int id) {
             if (id == 1)
@@ -27,6 +37,25 @@ namespace WebApplication1.Controllers
                 return Ok("Malewski");
             }
             return NotFound("Nie znaleziono studenta");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student) {
+            // ... adding to the database
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")] // update
+        public IActionResult UpdateStudent(int id)
+        {
+            return Ok("Aktualizacja zakonczona: " + id);
+        }
+
+        [HttpDelete("{id}")] // delete
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok("Usuwanie zakonczone" + id);
         }
     }
 }
---
+++ CW3_WebApplication1/WebApplication1/DAL/MockDbService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.DAL
+{
+    public class MockDbService : IDbService
+    {
+        private static IEnumerable<Student> _students;
+
+        static MockDbService() 
+        {
+            _students = new List<Student> 
+            {
+                new Student{ IdStudent = 1, FirstName = "Anna", LastName = "Nowak"},
+                new Student{ IdStudent = 2, FirstName = "Robert", LastName = "Kowalski"},
+                new Student{ IdStudent = 3, FirstName = "Jerzy", LastName = "Szczepaniak"}
+            };
+
+
+        }
+        public IEnumerable<Student> GetStudents()
+        {
+            return _students;
+        }
+    }
+}
